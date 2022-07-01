3d20634dea40feacf874ee9baab87c6df96a626f
Laura Santos G <lbsantosg@unal.edu.co>
2022-07-01 02:28:33 -0500

    add api first approach

--- Project/src/api.py
+++ Project/src/api.py
@@ -1,7 +1,32 @@
-def match_maker(COLE_COD_MCPIO_UBICACION,COLE_CALENDARIO,COLE_JORNADA,ESTU_GENERO,FAMI_TIENECOMPUTADOR,FAMI_TIENEINTERNET,FAMI_ESTRATOVIVIENDA,FAMI_CUARTOSHOGAR,FAMI_PERSONASHOGAR,FAMI_EDUCACIONPADRE,FAMI_EDUCACIONMADRE,FAMI_TRABAJOLABORPADRE,FAMI_TRABAJOLABORMADRE,Sociales,Naturales,Matematicas,Lectura,Ingles):
-    return [{'name':f'fakeSchool{i}', 'id': i }for i in range(0,10)]
+from functions import * 
+clean_school = [] #read from csv 
+
+def match_maker( **web_input ):
+    '''
+    1. Mapear inputs front => input modelos
+    2. Cargar el modelo y hacer la prediccion
+    3. Generar la huella del estudiante
+    4. Filtrar colegios (huella)
+    5. Match -> euclidean distance normalizada 
+    6. Retornar top n 
+    '''
+    print(f'\n********Answers from the user in the webform************:\n {web_input}\n\n')
+    model_input, parents_perception = map_inputs(**web_input)
+    print(f'********Data transformed to be used by the model************:\n {model_input}\n\n')
+    print(f'********Data of the parents perception:************\n {parents_perception}\n\n')
+    score_predictions = get_predictions(model_input)
+    print(f'********Predicted students score:************\n {score_predictions}\n\n')
+    student_print = get_student_print(score_predictions, parents_perception )
+    print(f'********Calculated student print:************\n {student_print}\n\n')
+    # top_schools = match_maker(student_print, clean_school[median_global]  )
+    # return [{'name':f'fakeSchool{i}', 'id': i }for i in range(0,10)]
+    return model_input, parents_perception
 
 def get_school(schoolId):
+    '''
+    1. Filtrar colegio por id
+    2. Retornar detalles del colegio 
+    '''
     return {
         'name': 'fakeSchool1',
         'id': '1234',
@@ -29,3 +54,26 @@ def get_school(schoolId):
             'languages': ['ALEMAN','INGLES']
         }
     }
+
+# TEST API
+mock_input = {
+    'mun_name' : 'Bogotá, D.C.',
+    'school_calendar': 'B', 
+    'school_shift': 'COMPLETA_UNICA', 
+    'student_gender': 'F', 
+    'has_pc': 'Si', 
+    'has_internet': 'Si',
+    'economic_stratus': 'Estrato 6' ,
+    'rooms_house': 'Dos',
+    'family_members': '3 a 4', 
+    'father_education': 'Educación profesional completa',
+    'mother_education': 'Postgrado',
+    'father_job': 'Pensionado', 
+    'mother_job': 'Es agricultor, pesquero o jornalero', 
+    'perception_socials': 3,
+    'perception_science': 5,
+    'perception_math': 5, 
+    'perception_reading': 4,
+    'perception_english': 3
+}
+test = match_maker(**mock_input)
